f1657e896c179f45039a9267d52fc51a6d0ebedb
phiineas <geek.jainil18@gmail.com>
2025-07-11 22:37:54 +0530

    duhh

--- src/app/delete/page.tsx
+++
@@ -1,6 +0,0 @@
-import MarkdownEditor from '@/components/MarkdownEditor';
-import ProjectPage from '@/components/ProjectPage';
-
-export default function Project() {
-  return <MarkdownEditor />;
-}
